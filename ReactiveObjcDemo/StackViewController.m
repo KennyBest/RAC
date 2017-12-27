3c40f68ac1207ce6c42ab6e9556e67488beb9a0d
llj <979520102@qq.com>
2017-12-27 18:30:45 +0800

    Network

--- ReactiveObjcDemo/StackViewController.m
+++ ReactiveObjcDemo/StackViewController.m
@@ -142,6 +142,7 @@
     
     
     RACSequence *scanSequence = [sequence1 scanWithStart:@"*" reduceWithIndex:^id _Nullable(id  _Nullable running, id  _Nullable next, NSUInteger index) {
+        // next: sequence中遍历对象，index：遍历索引
         NSString *current = sequence1.array[index];
         return [next stringByAppendingString:current];
     }];
@@ -156,6 +157,43 @@
      */
     NSLog(@"reduceWithIndex:%@", scanSequence.array);
     
+    // block返回YES则暂停 -- return @"1", @"2", @"3"
+    RACSequence *takeSequence1 = [sequence1 takeUntilBlock:^BOOL(id  _Nullable x) {
+        return [x integerValue] > 3;
+    }];
+    NSLog(@"-takeUntilBlock:%@", takeSequence1.array);
+    
+    // block返回NO则暂停 return empty sequence
+    RACSequence *takeSequence2 = [sequence1 takeWhileBlock:^BOOL(id  _Nullable x) {
+        return [x integerValue] > 3;
+    }];
+    NSLog(@"-takeWhileBlock:%@", takeSequence2.array);
+    
+    
+    // -skipUntilBlock: --以YES结束
+    // -skipWhileBlock: --以NO结束
+    
+    // -distinctUntilChanged --跟前一个值比较，
+    RACSequence *tmpSequence = [sequence1 distinctUntilChanged];
+    NSLog(@"-distinctUntilChanged:%@", tmpSequence.array);
+    tmpSequence = [tmpSequence skipUntilBlock:^BOOL(id  _Nullable x) {
+        return [x integerValue] > 3;
+    }];
+    NSLog(@"-distinctUntilChanged:%@", [tmpSequence distinctUntilChanged].array);
+    
+}
+
+- (IBAction)testNetType {
+    UIApplication *app = [UIApplication sharedApplication];
+    NSArray *nets = [[[app valueForKeyPath:@"statusBar"] valueForKeyPath:@"foregroundView"] subviews];
+    int netType = 0;
+    for (id child in nets) {
+        if ([child isKindOfClass:NSClassFromString(@"UIStatusBarDataNetworkItemView")]) {
+            netType = [[child valueForKeyPath:@"dataNetworkType"] intValue];
+            // 0: 无网络 1:2G  2:3G 3:4G 5:WIFI
+            NSLog(@"netType = %d", netType);
+        }
+    }
 }
 
 
